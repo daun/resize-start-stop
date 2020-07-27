1dfe6b55f807bedf8b1c6ab089bfa8b3e25b57b4
Philipp Daun <post@philippdaun.net>
2020-07-27 21:48:07 +0200

    Add tests

--- test/index.js
+++ test/index.js
@@ -1,9 +1,68 @@
 import { assert } from 'chai'
 
-import bindResizeEvents from '../src'
+import jsdom from 'jsdom-global'
 
-describe('Library', () => {
+jsdom()
+
+// Simulate window resize event
+const resizeEvent = document.createEvent('Event')
+resizeEvent.initEvent('resize', true, true)
+
+window.resizeTo = (width, height) => {
+  window.innerWidth = width || global.window.innerWidth
+  window.innerHeight = height || global.window.innerHeight
+  window.dispatchEvent(resizeEvent)
+}
+
+import bindResizeEvents, { toggleClassDuringResize } from '../src'
+bindResizeEvents()
+
+describe('Library', function () {
   it('exports a function', () => {
     assert(typeof bindResizeEvents === 'function', 'instance not a function')
+  })
+})
+
+describe('Events', function () {
+  this.timeout(1000)
+
+  it('triggers a resizestart event on resize', (done) => {
+    let triggered = false
+    window.addEventListener('resizestart', () => {
+      !triggered && done()
+      triggered = true
+    })
+    window.resizeTo(800, 1000)
+  })
+  it('triggers a resizestop after resize', (done) => {
+    let triggered = false
+    window.addEventListener('resizestop', () => {
+      !triggered && done()
+      triggered = true
+    })
+    window.resizeTo(600, 1000)
+  })
+})
+
+describe('Class names', function () {
+  this.timeout(1000)
+
+  const className = 'resize-classname-test'
+  toggleClassDuringResize({ className })
+
+  it('adds a classname on resize', () => {
+    window.resizeTo(800, 1000)
+    assert(
+      document.documentElement.classList.contains(className),
+      'class name was not added'
+    )
+  })
+  it('removes the classname after resize', (done) => {
+    window.resizeTo(600, 1000)
+    setTimeout(() => {
+      if (!document.documentElement.classList.contains(className)) {
+        done()
+      }
+    }, 500)
   })
 })
